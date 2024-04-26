f5a13bdc656be07209ad526a5d932f79677bdf78
praneethone <74635156+praneethone@users.noreply.github.com>
2024-04-26 11:53:34 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,8 @@
 select * from [deptdb].[dbo].[dept]
 
 select  count(*) from[HumanResources].[Department] where GroupName='Sales and Marketing'
+
+
+SELECT GroupName, COUNT(*) AS GroupCount
+FROM [HumanResources].[Department]
+GROUP BY GroupName;
