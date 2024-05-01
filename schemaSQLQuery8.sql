7217590dc8c1253c5a9b27c85f50ec7cc03b079d
praneethone <praneeth.cars@gmail.com>
2024-05-01 15:02:34 +0530

    newup

---
+++ schemaSQLQuery8.sql
@@ -0,0 +1,48 @@
+SELECT TOP (1000) [ID]
+      ,[FirstName]
+      ,[LastName]
+      ,[Gender]
+      ,[Salary]
+      ,[HireDate]
+  FROM [AdventureWorks2022].[dbo].[Employees]
+
+  drop procedure spEmployeecountbygender
+  create procedure spEmployeecountbygender
+  @Gender nvarchar(20),
+  @Employeecount int Output
+  as
+  begin
+	select @Employeecount=count(id)
+	from Employees
+	where Gender=@Gender
+  end 
+
+
+  declare @totalcount int
+  execute spEmployeecountbygender 'male', @totalcount out
+  print @totalcount
+
+  sp_helptext spEmployeecountbygender
+
+  sp_depends spEmployeecountbygender
+
+  create schema xyz
+  
+  create database xyz
+  
+  Create Table sample(
+  id int
+
+  );
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'remp')
+BEGIN
+    EXEC('CREATE SCHEMA remp')
+END
+
+create Table [remp].[sample](
+Id int,
+name varchar(50)
+)
+
+ 
